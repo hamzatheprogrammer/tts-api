4854b73d7e18092cd2c16550e314263cc1a63d1a
Hamza saleem <rydergamer1122@gmail.com>
2024-07-23 17:05:11 +0500

    "Added CORS middleware to index.js, updated package.json and package-lock.json to include cors dependency, and reformatted vercel.json"

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const client = require('soundoftext-js');
- 
 
+// Use CORS middleware
+app.use(cors());
 
 app.get('/speak/text=:text/lang=:lang', (req, res) => {
     const text = req.params.text;
     const lang = req.params.lang;
     client.sounds.create({ text: text, voice: lang })
-  .then(soundUrl => {
-    res.json({
-    "status": 200,
-    "developer": "hamza-saleem",
-    "response": soundUrl
-  });
-  })
-  .catch(e => {
-    res.json({
-        "status": 500,
-        "developer": "hamza-saleem",
-        "response": e
+    .then(soundUrl => {
+        res.json({
+            "status": 200,
+            "developer": "hamza-saleem",
+            "response": soundUrl
+        });
+    })
+    .catch(e => {
+        res.json({
+            "status": 500,
+            "developer": "hamza-saleem",
+            "response": e
+        });
     });
-  });
-    
-  
 });
 
-
-app.listen(3000, () => {
-  console.log('Server is running on http://127.0.0.1:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
